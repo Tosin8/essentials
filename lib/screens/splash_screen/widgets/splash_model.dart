327f2778ea930087203232ee6549c5059fa194f0
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-23 18:27:59 +0100

    Refactoring

---
+++ lib/screens/splash_screen/widgets/splash_model.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class SplashProduct {
+
+  String image;
+  SplashProduct({
+    
+    required this.image,
+  });
+}
+
+
+List <SplashProduct> splashProducts = [
+  SplashProduct(
+    
+    image: 'assets/images/splash/1.jpg',
+  ),
+  SplashProduct(
+  
+    image: 'assets/images/splash/2.jpg',
+  ),
+  SplashProduct(
+  
+    image: 'assets/images/splash/3.jpg',
+  ),
+  SplashProduct(
+    
+    image: 'assets/images/splash/4.jpg',
+  ),
+  SplashProduct(
+  
+    image: 'assets/images/splash/5.jpg',
+
+  ), 
+]; 
