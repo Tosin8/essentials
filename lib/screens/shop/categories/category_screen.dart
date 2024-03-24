d4c8ca67e9ed138b6b5f09f02c98aa795615817d
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-24 20:42:19 +0100

    Updating

--- lib/screens/shop/categories/category_screen.dart
+++ lib/screens/shop/categories/category_screen.dart
@@ -1,3 +1,4 @@
+import 'package:essentials/screens/shop/categories/widgets/category_model.dart';
 import 'package:flutter/material.dart';
 
 class CategoryScreen extends StatelessWidget {
@@ -9,10 +10,47 @@ class CategoryScreen extends StatelessWidget {
 
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        title: Text('Category'),
+        title: const Text('Category'),
 
       ),
-      
+      body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Padding(
+          padding:  const EdgeInsets.only(left: 8.0, right: 20.0),
+          child: Column(
+            children: [
+              Container(
+                height: 200, 
+                width: 375,  
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(image: (categories[0].image), fit: BoxFit.cover)),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: LinearGradient(colors: [
+                      Colors.transparent, 
+                      Colors.grey.withOpacity(0.7), 
+                
+                    ], 
+                    begin: Alignment.bottomLeft, 
+                    end: Alignment.topRight, 
+                    ), 
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      categories[0].title,
+                      categories[0].subTitle, 
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      )
+
     );
   }
 }
--- lib/screens/shop/categories/widgets/category_model.dart
+++ lib/screens/shop/categories/widgets/category_model.dart
@@ -1,14 +1,17 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class CategoryScreen {
   String title;
   String subTitle; 
   ImageProvider image; 
+  GestureTapCallback press; 
   CategoryScreen({
     required this.title,
     required this.subTitle,
     required this.image,
+    required this.press,
   });
 }
 
@@ -17,22 +20,23 @@ List categories = [
   CategoryScreen(
     title: 'T- SHIRTS',
     subTitle: '100 Products',
-    image: NetworkImage('https://picsum.photos/200'),
+    image: AssetImage('assets/images/category/1.jpg'), 
+    press: (){},
   ),
   CategoryScreen(
     title: 'SHOES',
     subTitle: '182 Products',
-    image: NetworkImage('https://picsum.photos/200'),
+    image: NetworkImage('https://picsum.photos/200'), press: () {  },
   ),
   CategoryScreen(
     title: 'FRAGNANCE',
     subTitle: '86 PRODUCTS',
-    image: NetworkImage('https://picsum.photos/200'),
+    image: NetworkImage('https://picsum.photos/200'), press: () {  },
   ),
   CategoryScreen(
     title: 'MAKE UPS',
     subTitle: '47 PRODUCTS',
-    image: NetworkImage('https://picsum.photos/200'),
+    image: NetworkImage('https://picsum.photos/200'), press: () {  },
   ),
   
 ]; 
