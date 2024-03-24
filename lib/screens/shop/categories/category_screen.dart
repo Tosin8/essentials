cb3f6ef1e93d9d0f2c45778e5284b021d8dc1e78
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-24 20:53:57 +0100

    Update category_screen.dart

--- lib/screens/shop/categories/category_screen.dart
+++ lib/screens/shop/categories/category_screen.dart
@@ -15,28 +15,54 @@ class CategoryScreen extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
-        child: Padding(
-          padding:  const EdgeInsets.only(left: 8.0, right: 20.0),
-          child: Column(
-            children: [
-              Container(
-                height: 200, 
-                width: 375,  
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(image: (categories[0].image), fit: BoxFit.cover)),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    gradient: LinearGradient(colors: [
-                      Colors.transparent, 
-                      Colors.grey.withOpacity(0.7), 
-                
-                    ], 
-                    begin: Alignment.bottomLeft, 
-                    end: Alignment.topRight, 
-                    ), 
-                  ),
+        child: ProductCard(),
+      )
+
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    super.key, this.title, this.subTitle, this.image,
+  });
+
+final CategoryScreen categories; 
+final title; 
+final subTitle;
+final image;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        
+      },
+      child: Padding(
+        padding:  const EdgeInsets.only(left: 8.0, right: 20.0),
+        child: Column(
+          children: [
+            Container(
+              height: 200, 
+              width: 375,  
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(image: (categories[0].image), fit: BoxFit.cover)),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  gradient: LinearGradient(colors: [
+                    Colors.transparent, 
+                    Colors.grey.withOpacity(0.7), 
+              
+                  ], 
+                  begin: Alignment.bottomLeft, 
+                  end: Alignment.topRight, 
+                  ), 
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.end,
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -52,12 +78,11 @@ class CategoryScreen extends StatelessWidget {
                     ],
                   ),
                 ),
-              )
-            ],
-          ),
+              ),
+            )
+          ],
         ),
-      )
-
+      ),
     );
   }
 }
