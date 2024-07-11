8e3a01e2caa8ab8972939580d2b3ae0201241e5f
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-11 20:43:17 +0100

    done with Nav. bar

--- lib/screens/shop/nav_bar.dart
+++ lib/screens/shop/nav_bar.dart
@@ -1,16 +1,20 @@
-
+import 'package:essentials/screens/shop/categories/category.dart';
 import 'package:flutter/material.dart';
+
+import 'cart.dart';
+import 'home.dart';
+import 'profile.dart';
+import 'saveitem.dart';
+
 
 class BottomNav extends StatefulWidget {
   @override
   _BottomNavState createState() => _BottomNavState();
 }
 
-class _BottomNavState extends State<BottomNav> with TickerProviderStateMixin {
+class _BottomNavState extends State<BottomNav> {
   int _selectedIndex = 0;
   PageController _pageController = PageController();
-  List<AnimationController> _animationControllers = [];
-  List<Animation<double>> _animations = [];
 
   static const List<Widget> _pages = <Widget>[
     HomeScreen(key: PageStorageKey('HomeScreen')),
@@ -20,40 +24,11 @@ class _BottomNavState extends State<BottomNav> with TickerProviderStateMixin {
     AccountScreen(key: PageStorageKey('AccountScreen')),
   ];
 
-  @override
-  void initState() {
-    super.initState();
-    for (int i = 0; i < 5; i++) {
-      AnimationController controller = AnimationController(
-        duration: const Duration(milliseconds: 300),
-        vsync: this,
-      );
-      _animationControllers.add(controller);
-      _animations.add(CurvedAnimation(parent: controller, curve: Curves.elasticOut));
-    }
-  }
-
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
       _pageController.jumpToPage(index);
-      _animateIcon(index);
     });
-  }
-
-  void _animateIcon(int index) {
-    _animationControllers[index].forward().then((_) {
-      _animationControllers[index].reverse();
-    });
-  }
-
-  @override
-  void dispose() {
-    for (var controller in _animationControllers) {
-      controller.dispose();
-    }
-    _pageController.dispose();
-    super.dispose();
   }
 
   @override
@@ -63,7 +38,7 @@ class _BottomNavState extends State<BottomNav> with TickerProviderStateMixin {
         children: [
           PageView(
             controller: _pageController,
-            physics: NeverScrollableScrollPhysics(),
+            physics: const NeverScrollableScrollPhysics(),
             children: _pages,
           ),
           Positioned(
@@ -71,13 +46,13 @@ class _BottomNavState extends State<BottomNav> with TickerProviderStateMixin {
             left: 0,
             right: 0,
             child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 16.0),
-              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              margin: const EdgeInsets.symmetric(horizontal: 16.0),
+              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
               decoration: BoxDecoration(
                 color: Colors.black,
                 borderRadius: BorderRadius.circular(30.0),
                 boxShadow: [
-                  BoxShadow(
+                  const BoxShadow(
                     color: Colors.black26,
                     blurRadius: 10,
                     spreadRadius: 5,
@@ -87,27 +62,42 @@ class _BottomNavState extends State<BottomNav> with TickerProviderStateMixin {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  _buildNavItem(Icons.home, 0),
-                  _buildNavItem(Icons.category, 1),
-                  SizedBox(width: 40), // Space for FAB
-                  _buildNavItem(Icons.save, 3),
-                  _buildNavItem(Icons.account_circle, 4),
+                  IconButton(
+                    icon: const Icon(Icons.home),
+                    color: _selectedIndex == 0 ? Colors.grey : Colors.white,
+                    onPressed: () => _onItemTapped(0),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.category),
+                    color: _selectedIndex == 1 ? Colors.grey : Colors.white,
+                    onPressed: () => _onItemTapped(1),
+                  ),
+                  const SizedBox(width: 40), // Space for FAB
+                  IconButton(
+                    icon: const Icon(Icons.save),
+                    color: _selectedIndex == 3 ? Colors.grey : Colors.white,
+                    onPressed: () => _onItemTapped(3),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.account_circle),
+                    color: _selectedIndex == 4 ? Colors.grey : Colors.white,
+                    onPressed: () => _onItemTapped(4),
+                  ),
                 ],
               ),
             ),
           ),
           Positioned(
-            bottom: 40.0,
+            bottom: 50.0,
             left: MediaQuery.of(context).size.width / 2 - 30,
             child: FloatingActionButton(
               onPressed: () {
                 setState(() {
                   _selectedIndex = 2; // Navigate to Cart page
                   _pageController.jumpToPage(2);
-                  _animateIcon(2);
                 });
               },
-              child: Icon(Icons.shopping_cart),
+              child: const Icon(Icons.shopping_cart),
             ),
           ),
         ],
@@ -115,78 +105,10 @@ class _BottomNavState extends State<BottomNav> with TickerProviderStateMixin {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
-
-  Widget _buildNavItem(IconData icon, int index) {
-    return GestureDetector(
-      onTap: () => _onItemTapped(index),
-      child: AnimatedBuilder(
-        animation: _animations[index],
-        builder: (context, child) {
-          return Transform.scale(
-            scale: _animations[index].value,
-            child: Icon(
-              icon,
-              color: _selectedIndex == index ? Colors.grey : Colors.white,
-              size: _selectedIndex == index ? 30 : 24,
-            ),
-          );
-        },
-      ),
-    );
-  }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Screen'),
-    );
-  }
-}
 
-class CategoryScreen extends StatelessWidget {
-  const CategoryScreen({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Category Screen'),
-    );
-  }
-}
 
-class CartScreen extends StatelessWidget {
-  const CartScreen({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Cart Screen'),
-    );
-  }
-}
-
-class SaveItemScreen extends StatelessWidget {
-  const SaveItemScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Save Item Screen'),
-    );
-  }
-}
-
-class AccountScreen extends StatelessWidget {
-  const AccountScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Account Screen'),
-    );
-  }
-}
---
+++ lib/screens/shop/saveitem.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SaveItemScreen extends StatelessWidget {
+  const SaveItemScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('Save Item Screen'),
+      ),
+    );
+  }
+}
