e6deaa258ff99f8fefec2353b3543cfc31316917
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-12 03:31:27 +0100

    done with category modeling

--- lib/screens/shop/categories/widgets/category_model.dart
+++ lib/screens/shop/categories/widgets/category_model.dart
@@ -16,13 +16,49 @@ class CategoryScreen {
 }
 
 
-List categories = [
+List categories =  [
   CategoryScreen(
-    title: 'T- SHIRTS',
+    title: 'Clothes',
     subTitle: '100 Products',
-    image: AssetImage('assets/images/category/1.jpg'), 
+    image: const AssetImage('assets/images/category/1.jpg'), 
     onTap: () {  }, 
   ),
-
-  
+ CategoryScreen(
+    title: 'Perfumes',
+    subTitle: '100 Products',
+    image: const AssetImage('assets/images/category/2.jpg'), 
+    onTap: () {  }, 
+  ),
+  CategoryScreen(
+    title: 'Shoes',
+    subTitle: '100 Products',
+    image: const AssetImage('assets/images/category/3.jpg'), 
+    onTap: () {  }, 
+  ),
+  CategoryScreen(
+    title: 'Cosmetics',
+    subTitle: '100 Products',
+    image: const AssetImage('assets/images/category/4.jpg'), 
+    onTap: () {  }, 
+  ),CategoryScreen(
+    title: 'Sunglasses',
+    subTitle: '100 Products',
+    image: const AssetImage('assets/images/category/5.jpg'), 
+    onTap: () {  }, 
+  ),CategoryScreen(
+    title: 'WristWatches',
+    subTitle: '100 Products',
+    image: const AssetImage('assets/images/category/6.jpg'), 
+    onTap: () {  }, 
+  ),CategoryScreen(
+    title: 'Bags',
+    subTitle: '100 Products',
+    image: const AssetImage('assets/images/category/7.jpg'), 
+    onTap: () {  }, 
+  ),CategoryScreen(
+    title: 'Accessories',
+    subTitle: '100 Products',
+    image: const AssetImage('assets/images/category/8.jpg'), 
+    onTap: () {  }, 
+  ),
 ]; 
